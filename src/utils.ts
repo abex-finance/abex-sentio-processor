fa8095dd9c8895178fd7b266ad8c29e1493a8dde
abex <abex@abex.fi>
2023-12-01 02:01:51 +0800

    Fix data

--- src/utils.ts
+++ src/utils.ts
@@ -7,9 +7,10 @@ export interface IToken {
 
 export const parseSuiTypeToToken = async (coinType: string, ctx: SuiContext|SuiObjectContext): Promise<IToken> => {
   const metaInfo = await ctx.client.getCoinMetadata({ coinType })
-  const name = metaInfo?.name.startsWith('Wrapped') && metaInfo?.symbol.startsWith('W') ? metaInfo?.symbol.slice(1) : metaInfo?.symbol
+  const name = metaInfo?.name.startsWith('Wrapped') && metaInfo?.symbol.startsWith('W') && metaInfo?.symbol.length >= 4 ? metaInfo?.symbol.slice(1) : metaInfo?.symbol
+  const decimal = metaInfo?.decimals
   return {
     name: name?.toLowerCase() || 'unknown',
-    decimal: metaInfo?.decimals || 9
+    decimal: decimal === undefined ? 9 : decimal
   }
 }
